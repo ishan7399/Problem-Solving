2e234daba750cd811e9238c009bf815d1f55def6
ishan7399 <99709273+ishan7399@users.noreply.github.com>
2022-09-19 13:11:49 +0530

    Time: 1 ms (15.20%), Space: 44 MB (16.07%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums.length==0)return 0;
+        int i;
+        for( i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                break;
+            }
+         else{
+            if(nums[i]>target){
+                break;
+            }
+         }
+        }
+        return i;
+    }
+}
