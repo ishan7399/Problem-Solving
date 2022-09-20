d13319dffa8708bcd36c17efd96ae0d5c99d9341
ishan7399 <99709273+ishan7399@users.noreply.github.com>
2022-09-20 11:48:47 +0530

    Time: 2 ms (84.27%), Space: 41.6 MB (27.57%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int mySqrt(int x) {
+        double y = Math.sqrt(x);
+        int ans =(int) Math.round(y-0.5);
+        if(ans> Integer.MAX_VALUE/10 )return 0;
+        return ans;
+    }
+}
